80b081bfbebf0a606582272ea8e9330995f9b183
Sameer2244 <31188576+Sameer2244@users.noreply.github.com>
2023-05-01 13:09:20 +0530

    2 files modified

--- src/Login.js
+++ src/Login.js
@@ -7,16 +7,14 @@ export default function Login() {
     /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,20}$/;
   const login = (e) => {
     e.preventDefault();
-    if (emailregex.test(email)) {
-      setError('Valid email address');
+    if (email.length === 0) {
+      alert('please enter email');
+    } else if (password.length === 0) {
+      alert('please enter password');
+    } else if (!emailregex.test(email) || !passwordregex.test(password)) {
+      alert('Invalid email or password');
     } else {
-      setError('Invalid email address');
-    }
-
-    if (passwordregex.test(password)) {
-      setError((e) => e + ' invalid password');
-    } else {
-      setError((e) => e + ' valid password');
+      alert('login successful');
     }
   };
 
@@ -26,32 +24,36 @@ export default function Login() {
   return (
     <div className="login-container">
       <div className="login-form">
-        <p>devtools.tech</p>
+        <p className="website-name">devtools.tech</p>
         <h1>Welcome to Devtools Tech</h1>
-        <p>Login</p>
+        <p className="login-title">Login</p>
         <div className="inputs-container">
           <form>
-            <input
-              type="text"
-              placeholder="Email"
-              value={email}
-              onChange={(e) => {
-                setEmail(e.target.value);
-              }}
-            />
-            <input
-              type="password"
-              placeholder="Password"
-              value={password}
-              onChange={(e) => {
-                setPassword(e.target.value);
-              }}
-            />
+            <div>
+              <input
+                type="text"
+                placeholder="Email"
+                value={email}
+                onChange={(e) => {
+                  setEmail(e.target.value);
+                }}
+              />
+            </div>
+            <div>
+              <input
+                type="password"
+                placeholder="Password"
+                value={password}
+                onChange={(e) => {
+                  setPassword(e.target.value);
+                }}
+              />
+            </div>
             <div className="forget-remember">
-              <span>
+              <div>
                 <input type="checkbox" />
                 Remember me
-              </span>
+              </div>
               <a>Forget password?</a>
             </div>
             <button onClick={login}>Login</button>
